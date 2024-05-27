b51891d2455b6cba1f6c890727759a898af4231e
aakri0 <139072694+aakri0@users.noreply.github.com>
2024-05-27 23:31:55 +0530

    updated game logic

--- src.cpp
+++ src.cpp
@@ -20,20 +20,40 @@ int main() {
     cin >> user;
 
     //Game Logic
-    if(user == "R" && v[rnd] == "S") {
-        cout << "Computer's choice: " << v[rnd] << endl;
-        cout << "You Win!" << endl;
-    } else if(user == "P" && v[rnd] == "R") {
-        cout << "Computer's choice: " << v[rnd] << endl;
-        cout << "You Win!" << endl;
-    } else if(user == "S" && v[rnd] == "P") {
-        cout << "Computer's choice: " << v[rnd] << endl;
-        cout << "You Win!" << endl;
-    } else if(user == v[rnd]) {
-        cout << "Computer's choice: " << v[rnd] << endl;
-        cout << "It's a Tie!" << endl;
+    int n = 5;
+    int user_score = 0;
+    int cmp_score = 0;
+    while(n--) {
+        if(user == "R" && v[rnd] == "S") {
+            cout << "Computer's choice: " << v[rnd] << endl;
+            cout << "You Win!" << endl;
+            user_score++;
+        } else if(user == "P" && v[rnd] == "R") {
+            cout << "Computer's choice: " << v[rnd] << endl;
+            cout << "You Win!" << endl;
+            user_score++;
+        } else if(user == "S" && v[rnd] == "P") {
+            cout << "Computer's choice: " << v[rnd] << endl;
+            cout << "You Win!" << endl;
+            user_score++;
+        } else if(user == v[rnd]) {
+            cout << "Computer's choice: " << v[rnd] << endl;
+            cout << "It's a Tie!" << endl;
+        } else {
+            cout << "Computer's choice: " << v[rnd] << endl;
+            cout << "You Lose!" << endl;
+            cmp_score++;
+        }
+    }
+
+    //Displaying the final score
+    cout << "User's Score: " << user_score << endl;
+    cout << "Computer's Score: " << cmp_score << endl;
+    if(user_score > cmp_score) {
+        cout << "You Win the Game!" << endl;
+    } else if(user_score < cmp_score) {
+        cout << "Computer Wins the Game!" << endl;
     } else {
-        cout << "Computer's choice: " << v[rnd] << endl;
-        cout << "You Lose!" << endl;
+        cout << "It's a Tie!" << endl;
     }
 }
