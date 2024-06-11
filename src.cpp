69e8e3241d98c81b8102cd716f993c5513c74ed8
aakri0 <139072694+aakri0@users.noreply.github.com>
2024-06-11 12:38:56 +0530

    updated game logic

--- src.cpp
+++ src.cpp
@@ -5,11 +5,6 @@ using namespace std;
 
 int main() {
     cout << "Welcome to Rock, Paper and Scissors" << endl;
-    int lb = 0, ub = 2;
-
-    //Code to generate a random integer between 1 and 3
-    srand(time(0));
-    int rnd = (rand() % 3) + 1;
 
     //Vector storing the choices
     vector<string> v = {"","R", "P", "S"};
@@ -18,10 +13,19 @@ int main() {
     int n = 5;
     int user_score = 0;
     int cmp_score = 0;
+
+    srand(time(0));
+
     while(n--) {
+        //Code to generate random integer between 1 and 3
+        int rnd = (rand() % 3)+1;
+
+        //Taking the user input
         string user;
         cout << "Enter your choice:(R, P and S) ";
         cin >> user;
+
+        //Checking the winner
         if(user == "R" && v[rnd] == "S") {
             cout << "Computer's choice: " << v[rnd] << endl;
             cout << "You Win!" << endl;
@@ -44,6 +48,7 @@ int main() {
         }
     }
 
+    cout << endl << endl;
     //Displaying the final score
     cout << "User's Score: " << user_score << endl;
     cout << "Computer's Score: " << cmp_score << endl;
